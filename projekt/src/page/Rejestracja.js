39e8c4b651f51d27a340c92df2dd670f3518544f
lukaszkot13 <biuro.opolelubelskie@gmail.com>
2021-09-28 20:36:29 +0200

    zmiany rejestracja

--- projekt/src/page/Rejestracja.js
+++ projekt/src/page/Rejestracja.js
@@ -3,65 +3,103 @@ import * as React from "react";
 import { Box, Button } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import axios from "axios";
+import { useState, useEffect } from "react";
 
 const Header = styled.form`
-  margin-top: 10%;
-  display: flex;
-  flex-direction: column;
-  justify-content: start;
-  align-items: center;
+	margin-top: 10%;
+	display: flex;
+	flex-direction: column;
+	justify-content: start;
+	align-items: center;
 `;
 const ButtonClick = styled.form`
-  margin-top: 10%;
-  display: flex;
-  flex-direction: column;
-  justify-content: start;
-  align-items: center;
+	margin-top: 10%;
+	display: flex;
+	flex-direction: column;
+	justify-content: start;
+	align-items: center;
 `;
 
 function Rejestracja() {
-  const DodajOsobe = () => {
-    const example = {
-      id: "{}",
-      Name: "{}",
-      Surname: "{}",
-      email: "{}",
-      password: "{}",
-    };
-    axios.post(`http://localhost:3000/dane`, example);
-  };
+	const [users, setUsers] = useState(null);
+	const [name, setName] = useState("");
+	const [surname, setSurname] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
-  return (
-    <from>
-      <Header>
-        <Box
-          component="form"
-          sx={{
-            "& .MuiTextField-root": { m: 1, width: "25ch" },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <div>
-            <TextField Name id="Name" label="Name" />
-            <TextField Surname id="Surname" label="Surname" />
-            <TextField
-              id="password"
-              label="Password"
-              type="password"
-              autoComplete="password"
-            />
-            <TextField id="email" label="e-mail" />
-          </div>
-        </Box>
-      </Header>
-      <ButtonClick>
-        <Button onClick={DodajOsobe} variant="contained" disableElevation>
-          Zarejestruj
-        </Button>
-      </ButtonClick>
-    </from>
-  );
+	useEffect(() => {
+		axios.get("http://localhost:3000/users").then((res) => {
+			setUsers(res.data);
+		});
+	}, []);
+
+	const changeName = (event) => setName(event.target.value);
+	const changeSurname = (event) => setSurname(event.target.value);
+	const changeEmail = (event) => setEmail(event.target.value);
+	const changePassword = (event) => setPassword(event.target.value);
+
+	const DodajOsobe = (users) => {
+		const example = {
+			name: name,
+			surname: surname,
+			email: email,
+			password: password,
+		};
+
+		axios.post(`http://localhost:3000/users`, example);
+	};
+
+	return (
+		<from>
+			<Header>
+				<Box
+					component="form"
+					sx={{
+						"& .MuiTextField-root": { m: 1, width: "25ch" },
+					}}
+					noValidate
+					autoComplete="off"
+				>
+					<div>
+						<TextField
+							Name
+							id="Name"
+							label="Name"
+							onChange={changeName}
+						/>
+						<TextField
+							secondName
+							id="secondName"
+							label="secondName"
+							onChange={changeSurname}
+						/>
+						<TextField
+							Password
+							id="password"
+							label="Password"
+							type="password"
+							onChange={changePassword}
+						/>
+						<TextField
+							Email
+							id="email"
+							label="e-mail"
+							onChange={changeEmail}
+						/>
+					</div>
+				</Box>
+			</Header>
+			<ButtonClick>
+				<Button
+					onClick={DodajOsobe}
+					variant="contained"
+					disableElevation
+				>
+					Zarejestruj
+				</Button>
+			</ButtonClick>
+		</from>
+	);
 }
 
 export default Rejestracja;
